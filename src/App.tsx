939bd84e49b00e8dda875ac1760796e9d55928e1
Dino <dinopranjic92@gmail.com>
2023-01-31 15:30:59 -0500

    Add Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Projects from './components/Projects';
 import Bio from './components/Bio';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Projects />
       <Bio />
     </div>
   );
