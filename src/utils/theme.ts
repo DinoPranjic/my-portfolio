a4d805e4a90357ffd1c6b6df59e5b0284f866528
Dino <dinopranjic92@gmail.com>
2023-02-01 19:52:50 -0500

    Change colour

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,7 +2,7 @@ const theme = {
   colours: {
     purplePrimary: '#732AEB',
     purpleAccent: 'rgba(115, 42, 235, 0.22)',
-    purpleBackground: '#E0D0FB',
+    purpleBackground: 'rgba(224, 208, 251, 0.17);',
     font: '#151515'
   },
   fonts: {
