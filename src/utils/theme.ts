53281daaca90b4d754f80ce2f274f3c71870170f
Dino <dinopranjic92@gmail.com>
2023-02-01 20:22:03 -0500

    Add hover colour

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,7 +3,8 @@ const theme = {
     purplePrimary: '#732AEB',
     purpleAccent: 'rgba(115, 42, 235, 0.22)',
     purpleBackground: 'rgba(224, 208, 251, 0.17);',
-    font: '#151515'
+    font: '#151515',
+    buttonHover: '#511da5'
   },
   fonts: {
     main: 'Arimo',
