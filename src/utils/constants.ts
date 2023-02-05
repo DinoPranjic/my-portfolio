71865cf5afb9195d660016c7c52313e9970ce511
Dino <dinopranjic92@gmail.com>
2023-02-04 22:15:57 -0500

    Create constants file and add Accent constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import theme from "./theme";
+
+const Accent = styled.span`
+  display: inline-block;
+  position: relative;
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: calc(100% + 10px);
+    height: 35%;
+    bottom: 5px;
+    left: -5px;
+    background-color: ${theme.colours.purpleAccent};
+  }
+`;
+
+export {
+  Accent
+}
