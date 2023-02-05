a093779ae2636086067a4f55b0d8c79f0800d5f2
Dino <dinopranjic92@gmail.com>
2023-02-05 11:54:53 -0500

    Import Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 import Bio from './components/Bio';
 import Contact from './components/Contact';
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <Projects />
+      <Skills />
       <Bio />
       <Contact />
     </div>
