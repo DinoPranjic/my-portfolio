55a8f27f17e25d7ca0df629f9b3ca31ad13818f3
Dino <dinopranjic92@gmail.com>
2023-02-05 11:55:40 -0500

    Import styled components and Wrapper

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,50 @@
+import styled from "styled-components";
+import Wrapper from "./Wrapper";
+import theme from "../utils/theme";
+import { Accent } from "../utils/constants";
+
+const SkillsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+`
+
+const SkillsHeader = styled.h2`
+  font-family: ${theme.fonts.secondary};
+  font-size: 36px;
+  color: ${theme.colours.font};
+  margin-bottom: 50px;
+`
+
+const stack = [
+  'React', 
+  'React Hooks',
+  'TypeScript',
+  'JavaScript',
+  'Styled Components',
+  'NodeJS',
+  'ExpressJS',
+  'Redux',
+  'Ruby',
+  'Ruby on Rails',
+  'SQL',
+  'Firebase',
+  'Git Version Control',
+  'REST API',
+  'HTML',
+  'CSS',
+  'SCSS'
+];
+
 const Skills = () => {
   return(
-    <>
-    </>
+    <Wrapper>
+      <SkillsHeader>
+        <Accent>Skills</Accent>
+      </SkillsHeader>
+
+    </Wrapper>
   )
-}
+};
+
+export default Skills;
