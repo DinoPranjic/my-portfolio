02b38c29c06ce8130cc1c4cf898356509c0dbc3d
Dino <dinopranjic92@gmail.com>
2023-02-05 20:13:22 -0500

    Add media queries

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,11 +1,21 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import theme from "../utils/theme";
 
 const WrapperContainer = styled.div`
   padding-left: 100px;
   padding-right: 100px;
   margin: 0 auto;
 
+  ${theme.mediaQuery.mobile} {
+    padding: 15px;
+    overflow-x: hidden;
+  }
+
+  ${theme.mediaQuery.tablet} {
+    padding: 50px;
+    overflow-x: hidden;
+  }
 `
 
 const Wrapper = ({ children }: {children: ReactNode}) => {
